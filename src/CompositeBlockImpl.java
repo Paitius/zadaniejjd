2789ce7f5cf02717bc0d5ce36058997af70557d3
Paitius <kgebka1996@gmail.com>
2023-12-16 16:39:58 +0100

    omówienie

--- src/CompositeBlockImpl.java
+++ src/CompositeBlockImpl.java
@@ -1,34 +1,55 @@
 import java.util.ArrayList;
 import java.util.List;
 public class CompositeBlockImpl implements CompositeBlock {
-
+    /**
+     * Zmienne ustawione na prywatne, dostęp poprzez gettery i settery
+     */
     private final List<Block> compositBlock = new ArrayList<>();
-
     private String color;
     private String material;
 
 
-
-
+    /**
+     * Konstruktor @CompositeBlockImpl tworzy pojedyńczy blok o parametrach:
+     * @param color
+     * @param material
+     */
     public CompositeBlockImpl(String color, String material){
         this.color = color;
         this.material = material;
     }
 
+    /**
+     * Drugi konstruktor, który przyjmuje jako parametr Listę pojedyńczych bloków
+     * dodaje do listy tworząc cały komponent
+     * @param elementsOfComposite
+     */
     public CompositeBlockImpl(List<Block> elementsOfComposite){
         this.compositBlock.addAll(elementsOfComposite);
     }
 
+    /**
+     *
+     * @return zwraca kolor bloku
+     */
     @Override
     public String getColor() {
         return color;
     }
 
+    /**
+     *
+     * @return zwraca materiał bloku
+     */
     @Override
     public String getMaterial() {
         return material;
     }
 
+    /**
+     *
+     * @return zwraca listę instancji tworzących dany komponent
+     */
     @Override
     public List<Block> getBlocks() {
         return compositBlock;
--- src/Wall.java
+++ src/Wall.java
@@ -5,14 +5,30 @@ public class Wall implements Structure{
 
     private final List<Block> wall;
 
+    /**
+     * Konstruktor budujący klasę Wall, podczas tworzenia klasy tworzy również pustą listę
+     */
     public Wall(){
-        this.wall = new ArrayList<>(1);
+        this.wall = new LinkedList<>();
     }
 
+    /**
+     /**
+     * Metoda klasy Wall, która pozwala na dodawanie elementów do listy budującej klasę Wall
+     * @param composite instancja klasy CompositeBlockImpl
+     **/
     public void addBlockToWall(Block composite){
         wall.add(composite);
     }
 
+    /**
+     *
+     * @param color poszukiwany kolor
+     * @return blockByColor Zwraca element z listy wall, która spełnia podany warunek koloru
+     * zwracany element jest Optional więc może być zwrócony null, gdyby takie element się nie znajdował
+     *
+     *Tworzymy strumień danych, które filtrujemy i powrównujemy kolor
+     */
     @Override
     public Optional<Block> findBlockByColor(String color) {
         Optional<Block> blockByColor = wall.stream()
@@ -21,12 +37,24 @@ public class Wall implements Structure{
 
         return blockByColor;
     }
-
+    /**
+     *
+     * @return zwraca listę instancji składającego się z materiału podanego w postaci
+     * listy typu <Block>
+     * Tworzy strumień danych z listy wall, na których jest wykonywana operacja sprawdzania
+     * czy dany @material znajduje się w bloku, następnie tworzy z tego listę, którą
+     * finalnie zwraca
+     **/
     @Override
     public List<Block> findBlocksByMaterial(String material) {
-        return wall.stream().filter(block -> block.getMaterial().contains(material)).collect(Collectors.toList());
+        return wall.stream()
+                .filter(block -> block.getMaterial().contains(material))
+                .collect(Collectors.toList());
     }
 
+    /**
+     * @return zwraca liczbę elementów w liście wall
+     */
     @Override
     public int count() {
         return wall.size();
